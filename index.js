749e25f22f31349e34fb832245067c0dde3cbd75
Chan102022 <raychristianmanicio9@gmail.com>
2025-11-21 14:29:59 +0800

    fix port for render

--- index.js
+++ index.js
@@ -85,5 +85,6 @@ app.post("/submit-result", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
+
